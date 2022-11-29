dc09854fffb209c22b1bce3f9b5945c5b93be554
Jack Graham <jack.blair.graham@gmail.com>
2022-11-29 14:45:09 -0600

    added WeywotSpringBlogApplication.java

---
+++ src/test/java/com/codeup/weywotspringblog/WeywotSpringBlogApplication.java
@@ -0,0 +1,15 @@
+package com.codeup.weywotspringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class WeywotSpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeywotSpringBlogApplication.class, args);
+    }
+
+}
