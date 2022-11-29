4192621a5d1d4fbef5a77f73430c2b8b10eb6ddc
Jack Graham <jack.blair.graham@gmail.com>
2022-11-29 14:30:05 -0600

    Customer.java updated

--- src/main/java/com/codeup/weywotspringblog/models/Customer.java
+++ src/main/java/com/codeup/weywotspringblog/models/Customer.java
@@ -16,14 +16,6 @@ public class Customer {
     @Column(nullable = false, length = 100)
     private String email;
 
-    public List<Coffee> getCoffeeList() {
-        return coffeeList;
-    }
-
-    public void setCoffeeList(List<Coffee> coffeeList) {
-        this.coffeeList = coffeeList;
-    }
-
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(
             name = "customers_coffees",
@@ -31,20 +23,6 @@ public class Customer {
             inverseJoinColumns = {@JoinColumn(name = "coffee_id")}
     )
     private List<Coffee> coffeeList;
-
-    public Customer() {
-    }
-
-    public Customer(String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
-
-    public Customer(long id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
 
     public long getID() {
         return id;
@@ -66,9 +44,30 @@ public class Customer {
         return email;
     }
 
-    public void setEmail() {
+    public void setEmail(String email) {
         this.email = email;
     }
 
+    public List<Coffee> getCoffeeList(){
+        return coffeeList;
+    }
+
+    public void setCoffeeList(List<Coffee> coffeeList){
+        this.coffeeList = coffeeList;
+    }
+
+    public Customer() {
+    }
+
+    public Customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public Customer(long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
 
 }
