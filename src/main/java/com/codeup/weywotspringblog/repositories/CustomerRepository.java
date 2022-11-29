f21269d6c66f387302ccaf6bbb84c888520b27c8
Jack Graham <jack.blair.graham@gmail.com>
2022-11-29 14:38:32 -0600

    CustomerRepository.java updated

--- src/main/java/com/codeup/weywotspringblog/repositories/CustomerRepository.java
+++ src/main/java/com/codeup/weywotspringblog/repositories/CustomerRepository.java
@@ -1,11 +1,8 @@
 package com.codeup.weywotspringblog.repositories;
 
 import com.codeup.weywotspringblog.models.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CustomerRepository {
-    default void save(Customer customer) {
-
-    }
-
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
     Customer findById(long customerId);
 }
